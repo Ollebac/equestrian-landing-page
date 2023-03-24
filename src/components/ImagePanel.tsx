d819fd92272fa7d7b7f66ceba3615dbd3a799830
Ian <iancabello16@gmail.com>
2023-03-24 01:19:45 -0400

    added image panels section

---
+++ src/components/ImagePanel.tsx
@@ -0,0 +1,40 @@
+import "../scss/image-panel.scss";
+
+const ImagePanel = () => {
+  return (
+    <div className="panel_container">
+      <div className="marquee_top">
+        <img src="marquee/marquee-1.jpg" className="marquee_image" />
+        <img src="marquee/marquee-2.jpg" className="marquee_image" />
+        <img src="marquee/marquee-3.jpg" className="marquee_image" />
+        <img src="marquee/marquee-4.jpg" className="marquee_image" />
+        <img src="marquee/marquee-5.jpg" className="marquee_image" />
+        <img src="marquee/marquee-6.jpg" className="marquee_image" />
+        <img src="marquee/marquee-1.jpg" className="marquee_image" />
+        <img src="marquee/marquee-2.jpg" className="marquee_image" />
+        <img src="marquee/marquee-3.jpg" className="marquee_image" />
+        <img src="marquee/marquee-4.jpg" className="marquee_image" />
+        <img src="marquee/marquee-5.jpg" className="marquee_image" />
+        <img src="marquee/marquee-6.jpg" className="marquee_image" />
+        <img src="marquee/marquee-1.jpg" className="marquee_image" />
+      </div>
+      <div className="marquee_bottom">
+        <img src="marquee/marquee-7.jpg" className="marquee_image" />
+        <img src="marquee/marquee-8.jpg" className="marquee_image" />
+        <img src="marquee/marquee-9.jpg" className="marquee_image" />
+        <img src="marquee/marquee-10.jpg" className="marquee_image" />
+        <img src="marquee/marquee-11.jpg" className="marquee_image" />
+        <img src="marquee/marquee-12.jpg" className="marquee_image" />
+        <img src="marquee/marquee-7.jpg" className="marquee_image" />
+        <img src="marquee/marquee-8.jpg" className="marquee_image" />
+        <img src="marquee/marquee-9.jpg" className="marquee_image" />
+        <img src="marquee/marquee-10.jpg" className="marquee_image" />
+        <img src="marquee/marquee-11.jpg" className="marquee_image" />
+        <img src="marquee/marquee-12.jpg" className="marquee_image" />
+        <img src="marquee/marquee-7.jpg" className="marquee_image" />
+      </div>
+    </div>
+  );
+};
+
+export default ImagePanel;
