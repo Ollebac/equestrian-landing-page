6dccefb554a8102cb0b226d66001141b7df51a1a
Ian <iancabello16@gmail.com>
2023-05-08 14:59:56 -0400

    more cleanup

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,127 +1,100 @@
-import "../scss/About/about.scss";
-// import "../scss/About/fonts.scss";
-import "../scss/fonts.scss";
+import '../scss/About/about.scss';
+import '../scss/fonts.scss';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 const playVideo = () => {
-  const button = document.getElementById("about_us_video_button");
-  button!.style.display = "none";
+  const button = document.getElementById('about_us_video_button');
+  button!.style.display = 'none';
   // button?.setAttribute("display", "none");
 
-  const iframe = document.getElementById("iframe_horse_video");
-  iframe?.setAttribute("src", iframe.getAttribute("src") + "&autoplay=1");
+  const iframe = document.getElementById('iframe_horse_video');
+  iframe?.setAttribute('src', iframe.getAttribute('src') + '&autoplay=1');
 };
 
 export const About = () => {
   useEffect(() => {}, []);
   return (
-    <section className="about_us_background" id="about">
-      <div className="about_us_content_container">
-        <h2 className="about_us_title headerLarge">About us</h2>
-        <h3 className="about_us_title_description sub_title">
+    <section className='about_us_background' id='about'>
+      <div className='about_us_content_container'>
+        <h2 className='about_us_title headerLarge'>About us</h2>
+        <h3 className='about_us_title_description sub_title'>
           Join us today and experience the joys of horseback riding!
         </h3>
 
-        <div className="about_us_video_container">
+        <div className='about_us_video_container'>
           <iframe
-            src="https://player.vimeo.com/video/469005678?h=5b3aa9c308&controls=1"
-            className="iframe_horse_video"
-            id="iframe_horse_video"
-            width="1500"
+            src='https://player.vimeo.com/video/469005678?h=5b3aa9c308&controls=1'
+            className='iframe_horse_video'
+            id='iframe_horse_video'
+            width='1500'
             height={400}
-            allow="autoplay; fullscreen; picture-in-picture"
+            allow='autoplay; fullscreen; picture-in-picture'
           ></iframe>
-          <button
-            onClick={playVideo}
-            className="about_us_video_button"
-            id="about_us_video_button"
-          >
-            <img
-              src="/icons/icon-play-button.png"
-              alt="Play Button"
-              className="play_button"
-            />
+          <button onClick={playVideo} className='about_us_video_button' id='about_us_video_button'>
+            <img src='/icons/icon-play-button.png' alt='Play Button' className='play_button' />
           </button>
-          <div className="iframe_horse_video_bottom"> </div>
+          <div className='iframe_horse_video_bottom'> </div>
         </div>
-        <div className="about_us_cards_container">
-          <div className="about_us_card">
-            <img src="/icons/icon-heart.svg" className="about_us_icon" />
-            <h3 className="about_us_card_title headerSmall">
-              Developing a strong bond with the horse
-            </h3>
-            <p className="about_us_card_description general_text">
-              Participating in an equestrian club provides riders with the
-              opportunity to form a strong and trusting bond with their horse.
-              This bond is formed through regular training, riding, and
-              socializing with the horse, which can be a rewarding experience.
+        <div className='about_us_cards_container'>
+          <div className='about_us_card'>
+            <img src='/icons/icon-heart.svg' className='about_us_icon' />
+            <h3 className='about_us_card_title headerSmall'>Developing a strong bond with the horse</h3>
+            <p className='about_us_card_description general_text'>
+              Participating in an equestrian club provides riders with the opportunity to form a strong and trusting
+              bond with their horse. This bond is formed through regular training, riding, and socializing with the
+              horse, which can be a rewarding experience.
             </p>
           </div>
-          <div className="about_us_card">
-            <img src="/icons/icon-checkmark.svg" className="about_us_icon" />
-            <h3 className="about_us_card_title headerSmall">
-              Learning new skills
-            </h3>
-            <p className="about_us_card_description general_text">
-              Equestrian clubs offer riders the chance to learn a variety of
-              skills and techniques, including dressage, show jumping and
-              cross-country riding. Riders can also learn about horse care,
-              nutrition, and other forms of horsemanship.
+          <div className='about_us_card'>
+            <img src='/icons/icon-checkmark.svg' className='about_us_icon' />
+            <h3 className='about_us_card_title headerSmall'>Learning new skills</h3>
+            <p className='about_us_card_description general_text'>
+              Equestrian clubs offer riders the chance to learn a variety of skills and techniques, including dressage,
+              show jumping and cross-country riding. Riders can also learn about horse care, nutrition, and other forms
+              of horsemanship.
             </p>
           </div>
-          <div className="about_us_card">
-            <img src="/icons/icon-people.svg" className="about_us_icon" />
-            <h3 className="about_us_card_title headerSmall">
-              Networking and camaraderie
-            </h3>
-            <p className="about_us_card_description general_text">
-              Joining an equestrian club provides members with a great
-              opportunity to meet other riders, exchange ideas, and make new
-              friends. Many clubs also host events and competitions which can be
-              a great way to meet people and develop new skills.
+          <div className='about_us_card'>
+            <img src='/icons/icon-people.svg' className='about_us_icon' />
+            <h3 className='about_us_card_title headerSmall'>Networking and camaraderie</h3>
+            <p className='about_us_card_description general_text'>
+              Joining an equestrian club provides members with a great opportunity to meet other riders, exchange ideas,
+              and make new friends. Many clubs also host events and competitions which can be a great way to meet people
+              and develop new skills.
             </p>
           </div>
         </div>
-        <div className="subscribe_container_cards">
-          <div className="subscribe_card_left">
+        <div className='subscribe_container_cards'>
+          <div className='subscribe_card_left'>
             <form
-              action=""
+              action=''
               // onSubmit={(e) => submitForm(e)}
-              className="email_form"
-              id="email_form"
+              className='email_form'
+              id='email_form'
             >
-              <h3 className="subscribe_title headerSmall">
-                Subscribe to the newsletter and get 5% discount on your first
-                training
+              <h3 className='subscribe_title headerSmall'>
+                Subscribe to the newsletter and get 5% discount on your first training
               </h3>
-              <div className="input_email">
-                <label htmlFor="email" className="form_label"></label>
+              <div className='input_email'>
+                <label htmlFor='email' className='form_label'></label>
                 <input
-                  type="email"
-                  id="form_input_email"
-                  placeholder="Your Email Address"
-                  name="email"
-                  className="general_text"
+                  type='email'
+                  id='form_input_email'
+                  placeholder='Your Email Address'
+                  name='email'
+                  className='general_text'
                   required
                 />
               </div>
-              <div className="email_form_btn_container">
-                <button
-                  type="submit"
-                  className="sign_up_btn general_text"
-                  id="sign_up_btn"
-                >
+              <div className='email_form_btn_container'>
+                <button type='submit' className='sign_up_btn general_text' id='sign_up_btn'>
                   Join us
                 </button>
               </div>
             </form>
           </div>
-          <img
-            src="/images/horse-subscribe.jpg"
-            alt="Horse Image"
-            className="subscribe_card_right"
-          />
+          <img src='/images/horse-subscribe.jpg' alt='Horse Image' className='subscribe_card_right' />
         </div>
       </div>
     </section>
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,61 +1,37 @@
-import "../scss/Welcome/welcome.scss";
-import "../scss/fonts.scss";
+import '../scss/Welcome/welcome.scss';
 
 const Welcome = () => {
   return (
     <>
-      <div className="welcome_container" id="home">
-        <div className="welcome_content_container">
-          <div className="welcome_content">
-            <p className="welcome_intro sub_title">Welcome to</p>
-            <div className="welcome_title_container">
-              <h1 className="welcome_title_1 site_header">Equestrian&nbsp;</h1>
-              <h1 className="welcome_title_2 site_header">club</h1>
+      <div className='welcome_section_container' id='home'>
+        <div className='welcome_content_container'>
+          <div className='welcome_content'>
+            <p className='welcome_intro'>Welcome to</p>
+            <div className='welcome_title_container'>
+              <h1 className='welcome_title_1'>Equestrian&nbsp;</h1>
+              <h1 className='welcome_title_2'>club</h1>
             </div>
-            <p className="welcome_text general_text">
-              We offer a variety of resources for horse enthusiasts of all
-              levels, from beginner to advanced.
+            <p className='welcome_text'>
+              We offer a variety of resources for horse enthusiasts of all levels, from beginner to advanced.
             </p>
-            <div className="welcome_button_container">
-              <a href="#join_us" className="welcome_button general_text">
+            <div className='welcome_button_container'>
+              <a href='#join_us' className='join_us_btn'>
                 Join us
               </a>
             </div>
           </div>
-          <div className="more_horses_outer_container">
-            <div className="more_horses_inner_container">
-              <img
-                src="/welcome/horse-profile-1.jpg"
-                className="horse_profile_img"
-                alt="Horse 1 Image"
-              />
-              <img
-                src="/welcome/horse-profile-2.jpg"
-                className="horse_profile_img"
-                alt="Horse 1 Image"
-              />
-              <img
-                src="/welcome/horse-profile-3.jpg"
-                className="horse_profile_img"
-                alt="Horse 1 Image"
-              />
+          <div className='more_horses_outer_container'>
+            <div className='more_horses_inner_container'>
+              <img src='/welcome/horse-profile-1.jpg' className='horse_profile_img' alt='Horse 1 Image' />
+              <img src='/welcome/horse-profile-2.jpg' className='horse_profile_img' alt='Horse 1 Image' />
+              <img src='/welcome/horse-profile-3.jpg' className='horse_profile_img' alt='Horse 1 Image' />
             </div>
-            <p className="more_horses_text general_text">
-              More than 20 horses for different levels of riders.
-            </p>
+            <p className='more_horses_text'>More than 20 horses for different levels of riders.</p>
           </div>
         </div>
-        <div className="welcome_image_container">
-          <img
-            src="/welcome/background-farm.jpg"
-            alt="farmhouse-background"
-            className="welcome_img"
-          />
-          <img
-            src="/welcome/horse-overlay.png"
-            alt="horse image"
-            className="horse_overlay"
-          />
+        <div className='welcome_image_container'>
+          <img src='/welcome/background-farm.jpg' alt='farmhouse-background' className='welcome_img' />
+          <img src='/welcome/horse-overlay.png' alt='horse image' className='horse_overlay' />
         </div>
       </div>
     </>
