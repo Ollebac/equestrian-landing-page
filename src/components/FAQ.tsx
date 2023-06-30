b2e207cf72a8439ff2fe0566fe3c40763467647d
Ian <iancabello16@gmail.com>
2023-06-30 19:39:01 -0400

    update faq font size and button icons

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,67 +1,64 @@
-import "../scss/FAQ/faq.scss";
-import "../scss/fonts.scss";
+import '../scss/FAQ/faq.scss';
+import '../scss/fonts.scss';
 
-import { navigateFAQ } from "../functions/navigate_FAQ";
+import { navigateFAQ } from '../functions/navigate_FAQ';
 
 const FAQ = () => {
   return (
-    <section className="faq_section_container" id="faq">
-      <div className="faq_title headerLarge">Frequently asked questions</div>
-      <ul className="faq_content_container">
-        <li className="faq_question_container">
+    <section className='faq_section_container' id='faq'>
+      <div className='faq_title headerLarge'>Frequently asked questions</div>
+      <ul className='faq_content_container'>
+        <li className='faq_question_container'>
           <button
-            className="faq_question headerMedium faq_question_inactive"
+            className='faq_question faq_question_inactive'
             onClick={() => {
               navigateFAQ(0);
             }}
           >
             Is this a subscription services?
           </button>
-          <p className="faq_answer general_text faq_answer_inactive">
-            No! The service provided is a one time fee, however there is a
-            general maintenance fee to keep the created website online. This fee
-            is charged by the host to run the servers.
+          <p className='faq_answer general_text faq_answer_inactive'>
+            No! The service provided is a one time fee, however there is a general maintenance fee to keep the created
+            website online. This fee is charged by the host to run the servers.
           </p>
         </li>
-        <li className="faq_question_container">
+        <li className='faq_question_container'>
           <button
-            className="faq_question headerMedium faq_question_inactive"
+            className='faq_question headerMedium faq_question_inactive'
             onClick={() => {
               navigateFAQ(1);
             }}
           >
             Can I customize the slides?
           </button>
-          <p className="faq_answer general_text faq_answer_inactive">
-            Of course! All slides are fully customizable to your specifications.
-            You will work directly with one of our designers to help fulfill
-            your requirements.
+          <p className='faq_answer general_text faq_answer_inactive'>
+            Of course! All slides are fully customizable to your specifications. You will work directly with one of our
+            designers to help fulfill your requirements.
           </p>
         </li>
-        <li className="faq_question_container">
+        <li className='faq_question_container'>
           <button
-            className="faq_question headerMedium faq_question_inactive"
+            className='faq_question headerMedium faq_question_inactive'
             onClick={() => {
               navigateFAQ(2);
             }}
           >
             What are the files inside?
           </button>
-          <p className="faq_answer general_text faq_answer_inactive">
-            Please provide all relevant files such as pictures, coach
-            descriptions, contact info, etc..
+          <p className='faq_answer general_text faq_answer_inactive'>
+            Please provide all relevant files such as pictures, coach descriptions, contact info, etc..
           </p>
         </li>
-        <li className="faq_question_container">
+        <li className='faq_question_container'>
           <button
-            className="faq_question headerMedium faq_question_inactive"
+            className='faq_question headerMedium faq_question_inactive'
             onClick={() => {
               navigateFAQ(3);
             }}
           >
             What kind of license will I get?
           </button>
-          <p className="faq_answer general_text faq_answer_inactive">
+          <p className='faq_answer general_text faq_answer_inactive'>
             You will get a license of full ownership of the domain.
           </p>
         </li>
--- src/functions/navigate_FAQ.tsx
+++ src/functions/navigate_FAQ.tsx
@@ -1,26 +1,14 @@
 export const navigateFAQ = (num: number) => {
-  let faq_question = document.getElementsByClassName("faq_question");
-  let faq_answer = document.getElementsByClassName("faq_answer");
+  let faq_question = document.getElementsByClassName('faq_question');
+  let faq_answer = document.getElementsByClassName('faq_answer');
 
-  if (faq_question[num].classList.contains("faq_question_inactive")) {
-    console.log("faq is inactive");
-    faq_question[num].className = faq_question[num].className.replace(
-      " faq_question_inactive",
-      " faq_question_active"
-    );
-    faq_answer[num].className = faq_answer[num].className.replace(
-      " faq_answer_inactive",
-      " faq_answer_active"
-    );
+  if (faq_question[num].classList.contains('faq_question_inactive')) {
+    console.log('faq is inactive');
+    faq_question[num].className = faq_question[num].className.replace(' faq_question_inactive', ' faq_question_active');
+    faq_answer[num].className = faq_answer[num].className.replace(' faq_answer_inactive', ' faq_answer_active');
   } else {
-    console.log("faq is active");
-    faq_question[num].className = faq_question[num].className.replace(
-      " faq_question_active",
-      " faq_question_inactive"
-    );
-    faq_answer[num].className = faq_answer[num].className.replace(
-      " faq_answer_active",
-      " faq_answer_inactive"
-    );
+    console.log('faq is active');
+    faq_question[num].className = faq_question[num].className.replace(' faq_question_active', ' faq_question_inactive');
+    faq_answer[num].className = faq_answer[num].className.replace(' faq_answer_active', ' faq_answer_inactive');
   }
 };
